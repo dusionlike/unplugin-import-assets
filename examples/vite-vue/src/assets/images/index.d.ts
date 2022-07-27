42ba9b4c0ab1aafff8dbbece94333146e45ad3d7
zhangzhixiong <751069244@qq.com>
2022-07-27 12:32:41 +0800

    chore: add vite-vue example

---
+++ examples/vite-vue/src/assets/images/index.d.ts
@@ -0,0 +1,8 @@
+declare module 'src/assets/images/car-ico.png' {
+  const ImgCarIco: string;
+  export default ImgCarIco;
+}
+declare module 'src/assets/images/in/idcard-ico.png' {
+  const ImgIdcardIco: string;
+  export default ImgIdcardIco;
+}
